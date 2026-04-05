dd6a76bcafcc1d3a6e6c8baf12db6cc3a799d2e3
som3a-dev <omar.eltayeb54@gmail.com>
2026-04-05 16:54:01 +0200

    dynamic image link retrieval route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,29 @@
 import express, { type Request, type Response } from 'express';
+import db, {getImageLink} from './database.ts'
 
 const app = express();
 
 // Routes
-app.route('/').get((req: Request, res: Response) => {
-  res.send('Hello Backend!');
+app.route('/images/:imageName').get(async (req: Request<{imageName: string}>, res: Response) => {
+  try {
+    const imageName = req.params.imageName;
+    const result = await getImageLink(imageName);
+    if (result) {
+      res.json({ 
+        message: "Found image",
+        imageUrl: result
+      });
+    }
+    else {
+      res.status(404).json({
+        message: "Image not found",
+      });
+    }
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
 });
 
 export default app;
