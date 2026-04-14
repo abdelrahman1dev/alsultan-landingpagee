e505d18cf8122ef7a04517f3417c1abe25118113
som3a-dev <omar.eltayeb54@gmail.com>
2026-04-15 01:27:50 +0200

    fix verifyPassword errors

--- backend/src/hash.ts
+++ backend/src/hash.ts
@@ -9,5 +9,13 @@ export async function hashPassword(password: string): Promise<string>
 
 export async function verifyPassword(hash: string, password: string) : Promise<boolean>
 {
+    if (typeof hash !== 'string' || typeof password !== 'string') {
+        return false;
+    }
+
+    if (hash.length === 0 || password.length === 0) {
+        return false;
+    }
+
     return await argon2.verify(hash, password);
 }
