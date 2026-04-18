872f02ab9b6902bd4d4d7806de5733b22847da61
som3a-dev <omar.eltayeb54@gmail.com>
2026-04-18 20:32:30 +0200

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,13 +11,10 @@ import { sessionObject } from './session.ts'
 
 const app = express();
 
-const corsOptions = {
+app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true
-};
-
-app.use(cors(corsOptions));
-
+  credentials: true 
+}));
 app.use(sessionObject);
 
 // Routes
