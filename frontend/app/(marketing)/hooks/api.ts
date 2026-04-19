d4e7783d804cc86652d5cf06ef413a5a2350aff2
som3a-dev <omar.eltayeb54@gmail.com>
2026-04-19 03:50:28 +0200

    create getSessionData

--- frontend/app/(marketing)/hooks/api.ts
+++ frontend/app/(marketing)/hooks/api.ts
@@ -4,3 +4,22 @@ import { API_URL } from '@/app/config/config';
 export const api = axios.create({
   baseURL: API_URL,
 });
+
+// Fetches session data from the api's /me route
+export async function getSessionData(): Promise<Object | null> {
+  try {
+    const res = await api.get('/me', {
+      withCredentials: true
+    });
+
+    if (!res.data.sessionData) {
+      throw new Error('Invalid /me response, status 200 but session data is undefined.');
+    }
+
+    return res.data.sessionData;
+  } catch (err: any) {
+    console.log(err);
+
+    return null;
+  }
+}
