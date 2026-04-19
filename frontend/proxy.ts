e28c95c8f94bb3db0cfac31d70aeb09e19fd0e6b
som3a-dev <omar.eltayeb54@gmail.com>
2026-04-19 03:32:47 +0200

    cleaner logged in checking

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -4,46 +4,38 @@ import { api } from './app/(marketing)/hooks/api';
 import { ur } from 'zod/v4/locales';
 import { requestToBodyStream } from 'next/dist/server/body-streams';
 
-export default async function proxy(request: NextRequest) {
-  const url = request.nextUrl.clone();
+async function isLoggedIn(request: NextRequest): Promise<boolean> {
   const sessionCookie = request.cookies.get('connect.sid');
 
-  if (url.pathname == '/login') {
-    if (sessionCookie) {
-      // The cookie could be for an invalid session, check /me to know if we are logged in
-      try {
-        const res = await api.get('/me', {
-          headers: {
-            Cookie: `connect.sid=${sessionCookie.value}`,
-          },
-        });
+  if (sessionCookie) {
+    try {
+      const res = await api.get('/me', {
+        headers: {
+          Cookie: `connect.sid=${sessionCookie.value}`,
+        },
+      });
 
-        url.pathname = '/';
-        return NextResponse.redirect(url);
-      } catch (err: any) {
-        console.log(err);
-      }
+      return true;
+    } catch (err: any) {
+      console.log(err);
+      return false;
+    }
+  }
+
+  return false;
+}
+
+export default async function proxy(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  const loggedIn = await isLoggedIn(request);
+
+  if (url.pathname == '/login') {
+    if (loggedIn) {
+      url.pathname = '/';
+      return NextResponse.redirect(url);
     }
   } else if (url.pathname.endsWith('/dashboard')) {
-    if (sessionCookie) {
-      try {
-        const res = await api.get('/me', {
-          headers: {
-            Cookie: `connect.sid=${sessionCookie.value}`,
-          },
-        });
-        // Logged in, let them access
-        console.log(res.status);
-      } catch (err: any) {
-        console.log(err);
-
-        // Don't let them access the dashboard, redirect
-        url.pathname = '/';
-        return NextResponse.redirect(url);
-      }
-    } else {
-      // Not logged in, redirect
-
+    if (!loggedIn) {
       url.pathname = '/';
       return NextResponse.redirect(url);
     }
