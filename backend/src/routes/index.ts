bebb280fc1ec66fd982147c8d6686d5871211b82
som3a-dev <omar.eltayeb54@gmail.com>
2026-04-27 08:19:28 +0300

    lectures router & :lectureId/videos route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,12 +3,14 @@ import coursesRouter from './courses/routes.ts';
 import userRouter from './user/routes.ts';
 import imagesRouter from './images/routes.ts';
 import videosRouter from './videos/routes.ts';
+import lecturesRouter from './lectures/routes.ts';
 
 const router = express.Router();
 
 router.use('/courses', coursesRouter);
 router.use('/images', imagesRouter);
 router.use('/videos', videosRouter);
+router.use('/lectures', lecturesRouter);
 router.use('/', userRouter);
 
 export default router;
---
+++ backend/src/routes/lectures/routes.ts
@@ -0,0 +1,28 @@
+import express, { type Request, type Response } from 'express';
+import * as db from '../../database.ts';
+import { ZodAny } from 'zod';
+
+const router = express.Router();
+
+router.route('/:lectureId/videos').get(async (req: Request, res: Response) => {
+  try {
+    const lectureId = req.params.lectureId;
+
+    if (typeof lectureId !== 'string') {
+      return res.status(401).json({ message: 'Invalid lecture ID parameter' });
+    }
+    if (/^\d+$/.test(lectureId) === false) {
+      return res.status(401).json({ message: 'Invalid lecture ID paramater' });
+    }
+
+    const data = await db.getLectureVideos(Number(lectureId));
+
+    return res
+      .status(200)
+      .json({ message: 'Found lecture videos', data: data });
+  } catch (err: any) {
+    return res.status(500).send();
+  }
+});
+
+export default router;
